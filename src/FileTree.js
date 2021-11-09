f18b0c3ee2b59edd815aaa2173f53845e8da1c40
Zach <>
2021-11-10 01:30:27 +1100

    File tree initialise

--- src/FileTree.js
+++ src/FileTree.js
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from 'react';
 
-function initialState(initialObj) {
-    const result = { ...initialObj };
+function initialState(cars) {
+    const result = { ...cars };
     Object.keys(results).forEach((key) => {
         const value = result[key];
         if (typeof value === 'object' && value !== null) {
             result[key].__isOpen = false;
         }
     })
+}
+
+function FileTree({ cars }) {
 }
 
 export default ObjectKeys;
