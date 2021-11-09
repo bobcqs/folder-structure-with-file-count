3a05070803bb07184a3e3936d48d90e114b6e06d
Zach <>
2021-11-10 00:04:36 +1100

    Find folder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
+export default function App() {
+  const [posts, setPosts] = useState([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("https://dev21.becollective.com/api/v2/coding-challenges/dirs")
+      .then((response) => {
+        console.log(response.data);
+        setPosts(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+        setError("we have received an error");
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        {posts.length
+          ? posts.map((post) => {
+              return <div key={post.id}>{post.name}</div>;
+            })
+          : null}
+      </div>
+      {error ? <div>{error}</div> : ""}
     </div>
   );
 }
-
-export default App;
---
+++ src/api/server.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import axios from 'axios'
+
+const axios = axios.create({
+    baseURL: 'https://dev21.becollective.com/api/v2/coding-challenges/dirs'
+})
+
+
+
+
+
